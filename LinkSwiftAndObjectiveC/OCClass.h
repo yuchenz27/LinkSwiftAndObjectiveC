c6cef4e1ad9517d5f2f6895154e977603486cde8
yuchen <yuchenz27@outlook.com>
2023-02-15 15:51:42 +0800

    added OCClass

---
+++ LinkSwiftAndObjectiveC/OCClass.h
@@ -0,0 +1,14 @@
+//
+//  OCClass.h
+//  LinkSwiftAndObjectiveC
+//
+//  Created by Yuchen Zhang on 2023/2/15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCClass : NSObject
+
+@property (
+
+@end
---
+++ LinkSwiftAndObjectiveC/OCClass.m
@@ -0,0 +1,14 @@
+//
+//  OCClass.m
+//  LinkSwiftAndObjectiveC
+//
+//  Created by Yuchen Zhang on 2023/2/15.
+//
+
+#import "OCClass.h"
+
+@implementation OCClass
+
+
+
+@end
